895f623b81e2046509c0286d15a1b7072d4b4a39
Hiranmay Das <hiranmay.das@cclab231.niser.ac.in>
2018-02-04 22:36:44 +0530

    1.a is done

--- lab5/1a.cpp
+++ lab5/1a.cpp
@@ -3,35 +3,35 @@ using namespace std;
 
 float* bubble(float* p,int size){
 	for(int i=0; i<size; i++){
-		for(int j=0; j<size-i;j++){
-			if(p[i]>p[i+1]){
-				float a =p[i];
+		for(int j=0; j<size-i-1;j++){
+			if(p[j]>p[j+1]){
+				float a =p[j];
 				p[j]=p[j+1];
 				p[j+1]=a;
 			}
 		}
 	}
-	cout<<"func"<<endl;
 	return p;
 }
 int main(){
-	float* arr;
-	float init=0;
-	arr=&init;
-	cout<<"initiali"<<endl;
 	cout<<"This is a program to implement bubble sort"<<endl;
 	cout<<"Please enter how much data you have"<<endl;
 	int num;
 	cin>>num;
+	float arr[num];
 	for(int i=0; i<num; i++){
 		cout<<"Data"<<i+1<<": ";
 		cin>>arr[i];
 	}	
-	arr=bubble(arr,num);
+	float *foo;
+	foo=arr;
+	foo=bubble(foo,num);
 	cout<<"Sorted data is as follows"<<endl;
 	for(int i=0; i<num; i++){
 		cout.width(10);
-		cout<<arr[i];
+		cout<<*foo;
+		foo++;
 	}
+	cout<<endl;
 	return 0;
 }
