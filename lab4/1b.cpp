9f3b19d123680165cabbb4760add4731dc061356
Hiranmay Das <hiranmay.das@niser.ac.in>
2018-02-28 00:21:27 +0530

    Comment added to all program

--- lab4/1b.cpp
+++ lab4/1b.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 
-class node{
+class node{//node class consisting only two element data and address of next node
 	public:
 	float data; 
 	node* next;
 };
-class queuell{
+class queuell{ //queuell class consisting of head and tail node pointer and push,pop and show function
 	node* head, *tail; 
 	public:
 	queuell(){
@@ -40,47 +40,47 @@ int main(){
 }
  
 void queuell::push(float data){
-	node* temp =new node;
-	temp ->data = data;
-	temp->next=NULL;
-	if (head ==NULL){
-		head =temp ;
+	node* temp =new node; //declaring a new object
+	temp ->data = data; //assign data of temp by data
+	temp->next=NULL; //making the next pointer of temp as NULL 
+	if (head ==NULL){ //if theere is no element in the queue
+		head =temp ; //pointing both head and tail to the new object
 		tail =temp;
 	}	
-	else {
+	else { //else attaching the element next to tail and then point tail to the new object
 		tail->next =temp;	
 		tail =temp;
 	}
-	cout<<"pushed"<<endl;
+	cout<<"pushed"<<endl; //confirm messege for pushing 
 }
-void queuell::pop(){
-	if (head==NULL){
+void queuell::pop(){ //pop function
+	if (head==NULL){ //if no element in queue
 		cout<<"This queue is empty you can't pop any more"<<endl;
 	}
-	else if(head==tail){
-		node* temp =head;
+	else if(head==tail){ //if only one element in queue
+		node* temp =head; //deleting the first element and pointing head and tail to NULL
 		delete temp;
 		head =NULL;
 		tail= NULL;
 		cout<<"popped"<<endl;
 	}
-	else {
-	 	node* temp =head;
-		head =head->next;
-		delete temp;
+	else { //if there is more than one element in linked list
+	 	node* temp =head;//pointing temp to 1st element
+		head =head->next; //pointing head to the 2nd element
+		delete temp; //deleting the 1st element
 		cout<<"popped"<<endl;
 	}
 }
-void queuell::show(){
+void queuell::show(){ //function to show the queue
 	node* temp =head;
-	if(head==NULL){
+	if(head==NULL){ //if the list is empty
 	cout<<"there is nothing to show"<<endl;
 	}
 	else {
-		while(temp!=tail){
-		cout<<temp->data<<"->";
+		while(temp!=tail){ //until the last element is reached 
+		cout<<temp->data<<"->";//showing the data of  elements
 		temp=temp->next;	
 		}
-		cout<<temp->data<<endl;
+		cout<<temp->data<<endl; //showing the data of last element
 	}
 }
--- lab4/1d.cpp
+++ lab4/1d.cpp
@@ -1,42 +1,42 @@
 #include<iostream>
 using namespace std;
 
-int count=0;
-int pos=0;
-void push(float* list);
-void pop(float* list);
-void show(float* list);
+int count=0; //declaring a global function to count the number of data
+int pos=0; 
+void push(float* list); //function to push
+void pop(float* list); //function to pop
+void show(float* list); //function to show
 
 int main(){
 	float* list;	
 	cout<<"NUMber of data to push:"<<endl;
 	int num;
 	cin>>num;
-	for(int i =0; i<num; i++){
-		push(list);
+	for(int i =0; i<num; i++){ //loop to push multiple times
+		push(list); //calling the push to insert data
 	}
 	cout<<"new stack"<<endl;
-	show(list);
-	cout<<"Number of data you want to pop"<<endl;
+	show(list); //showing the queue
+	cout<<"Number of data you want to pop"<<endl; 
 	cin>>num;
-	for(int i= 0; i<num; i++){
-		pop(list);
+	for(int i= 0; i<num; i++){ //loop for popping multiple times
+		pop(list); //calling pop function with reference to the first element of the queue
 	}
 	cout<<"new stack"<<endl;
-	show(list);
+	show(list); //shoeing the queue
 	
 	return 0;
 }
 void push(float* list){
 	cout<<"Data"<<count<<":";
 	float data;
-	cin>>data;
-	list[count]=data;
-	count++;
+	cin>>data; //taking data from the user 
+	list[count]=data; //inserting the data to queue
+	count++; //incrementing count to one as one item is added
 	cout<<"pushed"<<endl;
 }
 void pop(float* list){
-	pos++;
+	pos++; //pos is incremented, now the show  function will show the data after pos
 	cout<<"pooped"<<endl;
 	
 }
@@ -46,9 +46,9 @@ void show(float* list){
 		cout<<"There is nothing to show";
 	}
 	else{
-		float* temp=list;
-		for(int i=pos; i<count; i++){
-			cout<<temp[i]<<"->";
+		float* temp=list; 
+		for(int i=pos; i<count; i++){ //runnig loop number of times same as count-pos
+			cout<<temp[i]<<"->"; //showing the data after pos
 		}
 	}
 	cout<<endl;
